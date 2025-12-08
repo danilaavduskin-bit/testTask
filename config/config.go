5d0abb556d30cf7a990dfd11d15f294351939941
Danila <danilaavduskin-bit@example.com>
2025-12-08 15:06:54 +0300

    Update config Init

--- config/config.go
+++ config/config.go
@@ -1,39 +1,14 @@
 package config
 
-import (
-	"github.com/joho/godotenv"
-	"log"
-	"os"
-	"strconv"
-)
-
+// Решил остановиться на минимальном варианте без .env и сторонних библиотек
 type Config struct {
 	MaxMessages int32
 	MaxRetries  int
 }
 
-func EnvInit() *Config {
-	if err := godotenv.Load("./.env"); err != nil {
-		log.Fatal("Error loading .env file")
-		return nil
-	}
-
-	cfg := &Config{
-		MaxMessages: 200, // значения по умолчанию
+func NewConfig() *Config {
+	return &Config{
+		MaxMessages: 200,
 		MaxRetries:  3,
 	}
-
-	if val := os.Getenv("MAX_MSG"); val != "" {
-		if n, err := strconv.Atoi(val); err == nil && n > 0 {
-			cfg.MaxMessages = int32(n)
-		}
-	}
-
-	if val := os.Getenv("MAX_RETRY"); val != "" {
-		if n, err := strconv.Atoi(val); err == nil && n >= 0 {
-			cfg.MaxRetries = n
-		}
-	}
-
-	return cfg
 }
